a455b2379e8a516627d969ba4800aa4a0720e54e
SinaMegapolis <sinasaffari6@gmail.com>
2018-07-02 23:06:32 +0430

    trying to get rid of Proxies for good

--- src/main/java/sinamegapolis/moredyeablearmors/proxy/ClientRegistry.java
+++ src/main/java/sinamegapolis/moredyeablearmors/proxy/ClientRegistry.java
@@ -3,38 +3,21 @@ package sinamegapolis.moredyeablearmors.proxy;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import sinamegapolis.moredyeablearmors.MoreDyeableArmors;
 import sinamegapolis.moredyeablearmors.init.IHasModel;
 import sinamegapolis.moredyeablearmors.init.ModRegistry;
 
-public class ClientProxy extends CommonProxy {
-    @Override
-    public void preInit(FMLPreInitializationEvent e) {
-        super.preInit(e);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @Override
-    public void init(FMLInitializationEvent e) {
-        super.init(e);
-    }
-
-    @Override
-    public void postInit(FMLPostInitializationEvent e) {
-        super.postInit(e);
-    }
-
+@Mod.EventBusSubscriber(modid = MoreDyeableArmors.MODID, value = Side.CLIENT)
+public class ClientRegistry {
     @SubscribeEvent
-    public void onModelRegister(ModelRegistryEvent e) {
+    public static void onModelRegister(ModelRegistryEvent e) {
         for(net.minecraft.block.Block b  : ModRegistry.BLOCKS) if(b instanceof IHasModel) ((IHasModel) b).registerModels();
         for(Item i: ModRegistry.ITEMS) if(i instanceof IHasModel) ((IHasModel) i).registerModels();
-    }
-
-    @Override
-    public void registerRenderers(){
-
     }
 }
