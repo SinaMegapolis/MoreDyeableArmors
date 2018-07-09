9af4f19dfccb93376d2609af6860c711a0fa4121
SinaMegapolis <sinasaffari6@gmail.com>
2018-07-09 13:49:52 +0430

    fix build.gradle files (now "Actually" last commit before new release)

--- src/main/java/sinamegapolis/moredyeablearmors/MoreDyeableArmors.java
+++ src/main/java/sinamegapolis/moredyeablearmors/MoreDyeableArmors.java
@@ -15,7 +15,7 @@ public class MoreDyeableArmors
 {
     public static final String MODID = "moredyeablearmors";
     public static final String NAME = "More Dyeable Armors";
-    public static final String VERSION = "1.12.2-1.1.0";
+    public static final String VERSION = "1.12.2-1.2.0";
 
     public static final Logger logger = LogManager.getLogger(MoreDyeableArmors.MODID);
 
